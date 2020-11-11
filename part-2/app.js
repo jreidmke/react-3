3d27d2884fb13ef71a24a1464a3902ee83bafa7f
unknown <jreidmke@gmail.com>
2020-11-11 13:48:54 -0600

    complted app.js render

--- part-2/app.js
+++ part-2/app.js
@@ -1,9 +1,9 @@
 const App = () => {
     return(
         <div>
-            {/* Tweeet */}
-            {/* Tweeet */}
-            {/* Tweeet */}
+            <Tweet username='maria' name='Maria' date='01/24/89' tweet='This hot dog is-is good'/>
+            <Tweet username='james' name='James' date='11/26/96' tweet='This hamburger is-is good'/>
+            <Tweet username='oliver' name='Oliver' date='3/31/2012' tweet='This hot dog is-is good'/>
         </div>
     )
 }
