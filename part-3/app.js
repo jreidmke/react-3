0e0dbe86c34c68bdd5a1ad421a2c9b0fe3c07102
unknown <jreidmke@gmail.com>
2020-11-11 14:01:02 -0600

    completed rendering running scripts

--- part-3/app.js
+++ part-3/app.js
@@ -1,7 +1,9 @@
 const App = () => {
     return(
         <div>
-            {/* Person */}
+            <Person age={23} name='Maria' hobbies={['skateboard', 'being good', 'making snacks']}/>
+            <Person age={31} name='James' hobbies={['bike', 'being bad', 'making pizza']}/>
+            <Person age={5} name='Oliverrrrrrrrrrrr' hobbies={['being hilarious', 'walking', 'making mac n cheese']}/>
         </div>
     )
 }
